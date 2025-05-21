e82f9eb539a98e44b8e1733121f7596e18b98f1e
kmlplnc <mehmetkemal.palanci@std.antalya.edu.tr>
2025-05-21 16:02:36 +0300

    Fix: Update Vite config to bundle tailwind-merge

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: 'index.html',
-      external: ['@hookform/resolvers/zod', 'date-fns', 'class-variance-authority', 'tailwind-merge'],
+      external: ['@hookform/resolvers/zod', 'date-fns', 'class-variance-authority'],
       output: {
         manualChunks: {
           'vendor': [
